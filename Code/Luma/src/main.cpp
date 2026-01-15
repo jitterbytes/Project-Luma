284467ef568078c35b4bbd93d76d07f079813a68
sarvesh <133990569+jitterbytes@users.noreply.github.com>
2026-01-15 13:45:55 +0000

    updating main file comments

--- Code/Luma/src/main.cpp
+++ Code/Luma/src/main.cpp
@@ -1,18 +1,28 @@
+/**
+ * @file main.cpp
+ * @author sarvesh
+ * @brief Main application file for Luma - Polls Hardware Button and updates the FSM
+ * @version 1.0
+ * @date 2026-1-15
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
 #include <Arduino.h>
 #include "fsm.h"
 
-// Function Prototypes 
-void pollButtons();
+void pollButtons();     // Function to continously poll buttons for state change and interaction
 
 // Create one global FSM object for Class LumaFSM - has all the functions 
 LumaFSM fsm;
 
-// === Button & Debouncing Configs === 
-const int BUTTON_A_PIN = 5;     // GPIO 5 
-const int BUTTON_B_PIN = 2;     // GPIO 2
+// ==================== Button & Debouncing Configs ====================
+const int BUTTON_A_PIN = 5;          // Connected to GPIO 5 
+const int BUTTON_B_PIN = 2;          // Connected to GPIO 2
 
-const int DEBOUNCE_TIME = 20;       // 20ms
-const int LONG_PRESS_TIME = 1000;   // 1s
+const int DEBOUNCE_TIME = 20;        // Button Debounce 20ms
+const int LONG_PRESS_TIME = 1000;    // Hold for 1s for long press
 
 unsigned long buttonA_DownTime = 0;  // tracks the time for A
 unsigned long buttonB_DownTime = 0;  // tracks the time for B
@@ -31,43 +41,43 @@ void loop(){
   delay(20);        // 50 FPS update rate
 }
 
-// === Button Polling ===
+// ==================== Button Polling ====================
 void pollButtons() {
     
     // === Button A logic ===
-    bool buttonA_Now = digitalRead(BUTTON_A_PIN) == LOW; // Pressed == LOW -> then o/p 1 , Released == HIGH -> then o/p 0
+    bool buttonA_Now = digitalRead(BUTTON_A_PIN) == LOW;    // Pressed == LOW -> then o/p 1 , Released == HIGH -> then o/p 0
     
-    if (buttonA_Now && !buttonA_WasDown) {    // Button A just pressed
-        buttonA_DownTime = millis();         // Start timer 
+    if (buttonA_Now && !buttonA_WasDown) {                  // Button A just pressed
+        buttonA_DownTime = millis();                        // Start button down timer 
         buttonA_WasDown = true;
     } 
-    else if (!buttonA_Now && buttonA_WasDown) {   // Button A just released
-        unsigned long press_Duration = millis() - buttonA_DownTime;   // Calculate the press duration
+    else if (!buttonA_Now && buttonA_WasDown) {                         // Button A just released
+        unsigned long press_Duration = millis() - buttonA_DownTime;     // Calculate the press duration
         buttonA_WasDown = false;
         
-        if (press_Duration > DEBOUNCE_TIME) {        // Ignore any press less than Debounce time(20ms) - noise
+        if (press_Duration > DEBOUNCE_TIME) {           // Ignore any press less than Debounce time(20ms) - noise
             if (press_Duration > LONG_PRESS_TIME)  
-                fsm.onButtonAPressed(true);  // Long Press        
+                fsm.onButtonAPressed(true);             // Long Press        
             else                              
-                fsm.onButtonAPressed(false); // Short press      
+                fsm.onButtonAPressed(false);            // Short press      
         }
     }
     
     // === Button B logic ===
     bool buttonB_Now = digitalRead(BUTTON_B_PIN) == LOW; // Pressed == LOW -> then o/p 1 , Released == HIGH -> then o/p 0
     
-    if (buttonB_Now && !buttonB_WasDown) {    // Button B just pressed
-        buttonB_DownTime = millis();         // Start timer
+    if (buttonB_Now && !buttonB_WasDown) {              // Button B just pressed
+        buttonB_DownTime = millis();                    // Start button down timer
         buttonB_WasDown = true;
-    } else if (!buttonB_Now && buttonB_WasDown) {     // Button B just released
-        unsigned long press_Duration = millis() - buttonB_DownTime;   // Calculate the press duration
+    } else if (!buttonB_Now && buttonB_WasDown) {                       // Button B just released
+        unsigned long press_Duration = millis() - buttonB_DownTime;     // Calculate the press duration
         buttonB_WasDown = false;
         
         if (press_Duration > DEBOUNCE_TIME) {        // Ignore any press less than Debounce time(20ms) - noise   
             if (press_Duration > LONG_PRESS_TIME) 
-                fsm.onButtonBPressed(true); // Long Press
+                fsm.onButtonBPressed(true);         // Long Press
             else 
-                fsm.onButtonBPressed(false); // Short press 
+                fsm.onButtonBPressed(false);        // Short press 
         }
     }
 }
